53bc2d135c1523b909ad37804f2b1b69e74c2350
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2021-08-12 14:31:12 +0200

    Test sending back HTML as a response

--- server.js
+++ server.js
@@ -5,10 +5,27 @@ const bodyParser = require("body-parser");
 //email validator to use for customer endpoint
 const validator = require("email-validator");
 
+
+
+
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-
+app.get("/", function(req,res) {
+  res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Hello World</p>
+</body>
+</html>`);
+});
 
 
 app.route("/customers")
